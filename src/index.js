6f78929ba8cfceedc2c792b0b739a33634d8bc7f
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2020-10-23 05:43:08 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './components/App';
+import rootReducer from './reducers';
 
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action !== 'function') {
+    console.log('ACTION_TYPE', action.type);
+  }
+  next(action);
+};
+
+// const thunk = ({ dispatch, getState }) => (next) => (action) => {
+//   if (typeof action === 'function') {
+//     action(dispatch);
+//     return;
+//   }
+//   next(action);
+// };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+console.log('store', store);
+console.log('store', store.getState());
+
+export const StoreContext = createContext();
+
+class Provider extends React.Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
-
-
